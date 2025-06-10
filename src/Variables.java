900d19892832c3c49ef67f193bd6463fc0d96352
Смирнов Владислав <vladislav.a.smirnov@gazprombank.ru>
2025-06-10 16:59:00 +0500

    add variables examples

---
+++ src/Variables.java
@@ -0,0 +1,8 @@
+public class Variables {
+    public static void main(String[] args){
+        int one  = 1;
+        double someValue = 2.4343;
+        String string  = "Какая-то строка";
+        Boolean isCool = true;
+    }
+}
