252c1757bcd8b72059e23b4b05c074cf773664c4
Смирнов Владислав <vladislav.a.smirnov@gazprombank.ru>
2025-06-14 03:47:44 +0500

    4.16 Math class examples

--- src/primitives_and_objects/Main.java
+++ src/primitives_and_objects/Main.java
@@ -4,20 +4,37 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        //Math.random
+        double a = Math.random() * (double) 10;
+        System.out.println("Значение a: " + a);
+        System.out.println("целое значение a: " + (int) a);
+
+        //Math.random
+        double b = 3.7;
+        int roundedB = (int) Math.round(b);
+        System.out.println("округленное значение roundedB: " + roundedB);
+
+        //Math.random
+        int c = 9;
+        int squareRoot = (int) Math.sqrt(c);
+        System.out.println("квадратный корень с: " + squareRoot);
 
 
-        int a = 10 + 2;
-        int b = 10 - 2;
-        int c = 10 * 2;
-        int d = 10 / 2;
-        int e = 10 % 3;
+        //Math.random
+        int d = 9;
+        int degree = 20;
+        int result = (int) Math.pow(d,degree);
+        System.out.println("результат возведения в степень: " + result);
 
-        System.out.println("a = " + a);
-        System.out.println("b = " + b);
-        System.out.println("c = " + c);
-        System.out.println("d = " + d);
-        System.out.println("e = " + e);
+        /*
+        Math.max..
+        Math.min..
+        Math.PI..
+        Math.E..
 
-
+        итд
+         */
     }
+
+
 }
