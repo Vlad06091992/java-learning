47065dbc53a5d842615fcab975e304ad1eb7b934
Смирнов Владислав <vladislav.a.smirnov@gazprombank.ru>
2025-06-13 19:20:11 +0500

    4.11 number conversion

--- src/primitives_and_objects/Main.java
+++ src/primitives_and_objects/Main.java
@@ -2,15 +2,40 @@ package primitives_and_objects;
 
 public class Main {
     public static void main(String[] args) {
-        double a = 24.;
-        double b = 0.1;
+//     int a = 3;
+//     int b = 2;
+//
+//     //1
+//     int c = a / b;
+//     System.out.println(c);
 
-        // 2.4000000000000004
-        System.out.println(a * b);
+//        double a = 3;
+//        double b = 2;
+//
+//        //1.5
+//        double c = a / b;
+
+        int a = 3;
+        int b = 2;
+
+        //1.5, приведение a к double
+        double c = (double) a / b;
+
+        System.out.println(c);
+
+//
+//        //пример неверного приведения так как число не помещается в int
+//        long time = System.currentTimeMillis();
+//        System.out.println(time);
+//        System.out.println((int) time);
+
+        //пример верного приведения так как число помещается в int
+        long timeLong = System.currentTimeMillis();
+        long time = System.currentTimeMillis() / 1000;
+        System.out.println(timeLong);
+        System.out.println(time);
+        System.out.println((int) time);
 
 
-        //для точных вычислений используем
-        java.math.BigDecimal
-        java.math.BigInteger
     }
 }
