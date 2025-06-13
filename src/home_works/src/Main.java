42be4e70b4ca63edf21239c1271080a6b162b559
Смирнов Владислав <vladislav.a.smirnov@gazprombank.ru>
2025-06-13 20:00:40 +0500

    4.2 hw min and max value

--- src/home_works/src/Main.java
+++ src/home_works/src/Main.java
@@ -5,7 +5,9 @@ public class Main {
         Container container = new Container();
         container.count += 7843;
 
-        System.out.println(sumDigits(567));
+//        System.out.println(sumDigits(567));
+//        System.out.println(maxMinValue());
+        maxMinValue();
 
     }
 
@@ -20,5 +22,45 @@ public class Main {
         }
 
         return result;
+    }
+
+    public static void maxMinValue() {
+
+        byte a = 10;
+        System.out.println(a instanceof Byte);
+
+        int b = 10;
+        System.out.println(b instanceof Integer);
+
+        short c = 10;
+        System.out.println(c instanceof Short);
+
+        long d = 10;
+        System.out.println(d instanceof Long);
+
+        System.out.println(Byte.MAX_VALUE);
+        System.out.println(Byte.MIN_VALUE);
+
+        System.out.println(Integer.MAX_VALUE);
+        System.out.println(Integer.MIN_VALUE);
+
+        System.out.println(Short.MAX_VALUE);
+        System.out.println(Short.MIN_VALUE);
+
+        System.out.println(Long.MAX_VALUE);
+        System.out.println(Long.MIN_VALUE);
+
+        float e = 10;
+        System.out.println(e instanceof Float);
+
+        double f = 10;
+        System.out.println(f instanceof Double);
+
+
+        System.out.println(Float.MAX_VALUE);
+        System.out.println(Float.MIN_VALUE);
+
+        System.out.println(Double.MAX_VALUE);
+        System.out.println(Double.MIN_VALUE);
     }
 }
