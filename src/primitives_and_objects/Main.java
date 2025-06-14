c6ce4125c869be170a0ff68bae8c7d0fe0a06299
Смирнов Владислав <vladislav.a.smirnov@gazprombank.ru>
2025-06-14 19:54:06 +0500

    4.20 String methods

--- src/primitives_and_objects/Main.java
+++ src/primitives_and_objects/Main.java
@@ -4,10 +4,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String text = "какой-то\n \tтекст\n\u20AC";
+        String text = "какой-то текст";
+        System.out.println(text.length());
+        System.out.println(text.indexOf('о'));
+        System.out.println(text.lastIndexOf('о'));
 
+        int spaceIndex = text.indexOf(' ');
 
-        System.out.println(text);
+        System.out.println(text.substring(0,spaceIndex + 1));
+        System.out.println(text.substring(spaceIndex + 1));
+
+        String text_text = "     какой-то текст                 ";
+        System.out.println(text_text.trim());
+
     }
 
 
