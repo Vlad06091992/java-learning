9acda39b1080bd9b0a598a3023e24ee774d33c89
Смирнов Владислав <vladislav.a.smirnov@gazprombank.ru>
2025-06-14 19:48:08 +0500

    4.19 special symbols example

--- src/primitives_and_objects/Main.java
+++ src/primitives_and_objects/Main.java
@@ -4,12 +4,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String text = "какой-то текст";
-        char letter = text.charAt(0);
-        char letterTwo = 'л';
-        int code = (int) letter;
+        String text = "какой-то\n \tтекст\n\u20AC";
 
-        System.out.println(code);
+
+        System.out.println(text);
     }
 
 
