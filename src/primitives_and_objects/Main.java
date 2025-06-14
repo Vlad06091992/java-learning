41fd9203359ea9e03c9e4b0a94ba7dca2803d771
Смирнов Владислав <vladislav.a.smirnov@gazprombank.ru>
2025-06-14 19:45:12 +0500

    4.18 String, characters, encoding

--- src/primitives_and_objects/Main.java
+++ src/primitives_and_objects/Main.java
@@ -4,36 +4,12 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //Math.random
-        double a = Math.random() * (double) 10;
-        System.out.println("Значение a: " + a);
-        System.out.println("целое значение a: " + (int) a);
+        String text = "какой-то текст";
+        char letter = text.charAt(0);
+        char letterTwo = 'л';
+        int code = (int) letter;
 
-        //Math.random
-        double b = 3.7;
-        int roundedB = (int) Math.round(b);
-        System.out.println("округленное значение roundedB: " + roundedB);
-
-        //Math.random
-        int c = 9;
-        int squareRoot = (int) Math.sqrt(c);
-        System.out.println("квадратный корень с: " + squareRoot);
-
-
-        //Math.random
-        int d = 9;
-        int degree = 20;
-        int result = (int) Math.pow(d,degree);
-        System.out.println("результат возведения в степень: " + result);
-
-        /*
-        Math.max..
-        Math.min..
-        Math.PI..
-        Math.E..
-
-        итд
-         */
+        System.out.println(code);
     }
 
 
