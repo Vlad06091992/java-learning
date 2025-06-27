899952c6c1ba66ccdb28f6ae1a498ad384ba75b9
Смирнов Владислав <vladislav.a.smirnov@gazprombank.ru>
2025-06-27 11:16:20 +0500

    6.11 brief implementation of the interface

--- src/inheritance_and_polymorphism/interface_comparable_example/Main.java
+++ src/inheritance_and_polymorphism/interface_comparable_example/Main.java
@@ -1,25 +1,30 @@
 package inheritance_and_polymorphism.interface_comparable_example;
 
 import inheritance_and_polymorphism.Square;
-import inheritance_and_polymorphism.interface_comparator_example.SquareComparator;
-
+import java.util.Comparator;
 import java.util.TreeSet;
 
 public class Main {
 
-public static void main(String[] args) {
-        TreeSet<Square> squares = new TreeSet<>(new SquareComparator());
-        squares.add(new Square(40));
-        squares.add(new Square(20));
-        squares.add(new Square(10));
-        squares.add(new Square(15));
-        squares.add(new Square(41));
-        squares.add(new Square(32));
-        squares.add(new Square(28));
+        public static void main(String[] args) {
+                TreeSet<Square> squares = new TreeSet<>((square1, square2) -> {
+                        int squareFirst = square1.getSquare();
+                        int squareSecond = square2.getSquare();
 
-        for (Square square: squares){
-            System.out.println(square.getSquare());
+                        if (squareFirst > squareSecond) return 1;
+                        if (squareFirst < squareSecond) return -1;
+                        return 0;
+                });
+                squares.add(new Square(40));
+                squares.add(new Square(20));
+                squares.add(new Square(10));
+                squares.add(new Square(15));
+                squares.add(new Square(41));
+                squares.add(new Square(32));
+                squares.add(new Square(28));
+
+                for (Square square : squares) {
+                        System.out.println(square.getSquare());
+                }
         }
-
-    }
 }
