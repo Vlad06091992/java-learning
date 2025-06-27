5533f768af6b9913fe233bcdb5ab3f3a7131f907
Смирнов Владислав <vladislav.a.smirnov@gazprombank.ru>
2025-06-27 11:23:24 +0500

    6.12 map set interfaces examples

---
+++ src/inheritance_and_polymorphism/map_set_intefaces/Main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Map<String, String> treeMap = new TreeMap<>();
+        Map<String, String> hashMap = new HashMap<>();
+
+
+        Set<String> hashSet = new HashSet<>();
+        Set<String> treeSet = new TreeSet<>();
+
+    }
+}
