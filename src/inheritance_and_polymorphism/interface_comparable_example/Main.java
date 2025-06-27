5069dc19270ad7bb52460872d12f16f4a9792566
Смирнов Владислав <vladislav.a.smirnov@gazprombank.ru>
2025-06-27 10:35:41 +0500

    6.9 comparable interface example

---
+++ src/inheritance_and_polymorphism/interface_comparable_example/Main.java
@@ -0,0 +1,24 @@
+package inheritance_and_polymorphism.interface_comparable_example;
+
+import inheritance_and_polymorphism.Square;
+
+import java.util.TreeSet;
+
+public class Main {
+
+public static void main(String[] args) {
+        TreeSet<Square> squares = new TreeSet<>();
+        squares.add(new Square(40));
+        squares.add(new Square(20));
+        squares.add(new Square(10));
+        squares.add(new Square(15));
+        squares.add(new Square(41));
+        squares.add(new Square(32));
+        squares.add(new Square(28));
+
+        for (Square square: squares){
+            System.out.println(square.getSquare());
+        }
+
+    }
+}
