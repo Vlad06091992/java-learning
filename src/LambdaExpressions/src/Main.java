4fa1bded556f76a59b091e14d382f240b74d2999
Смирнов Владислав <vladislav.a.smirnov@gazprombank.ru>
2025-06-30 18:12:17 +0500

    7.1 lambda expression example

---
+++ src/LambdaExpressions/src/Main.java
@@ -0,0 +1,59 @@
+package LambdaExpressions.src;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+public class Main {
+    private static String staffFile = "src/LambdaExpressions/data/staff.txt";
+
+
+
+    private static String dateFormat = "dd.MM.yyyy";
+
+    public static void main(String[] args) {
+        System.out.println("Current working directory: " + System.getProperty("user.dir"));
+
+
+        ArrayList<Employee> staff = loadStaffFromFile();
+
+//        Collections.sort(staff, new Comparator<Employee>() {
+//
+//            @Override
+//            public int compare(Employee o1, Employee o2) {
+//                return o1.getSalary().compareTo(o2.getSalary());
+//            }
+//        });
+
+
+        Collections.sort(staff, (o1, o2) -> o1.getSalary().compareTo(o2.getSalary()));
+
+        for (Employee item : staff) {
+            System.out.println(item);
+        }
+
+    }
+
+    private static ArrayList<Employee> loadStaffFromFile() {
+        ArrayList<Employee> staff = new ArrayList<>();
+        try {
+            List<String> lines = Files.readAllLines(Paths.get(staffFile));
+            for (String line : lines) {
+                String[] fragments = line.split("\t");
+                if (fragments.length != 3) {
+                    System.out.println("Wrong line: " + line);
+                    continue;
+                }
+                staff.add(new Employee(
+                        fragments[0],
+                        Integer.parseInt(fragments[1]),
+                        (new SimpleDateFormat(dateFormat)).parse(fragments[2])
+                ));
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        return staff;
+    }
+}
